27522f4f3703a609494cf242b176750765c178f3
Chanyeop Jeong <ops3199@outlook.kr>
2020-05-02 23:20:27 +0900

    test

--- src/test/java/com/spring/qna/controller/test/QnaControllerTest.java
+++ src/test/java/com/spring/qna/controller/test/QnaControllerTest.java
@@ -1,6 +1,8 @@
 package com.spring.qna.controller.test;
 
-import java.util.Date;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,46 +10,29 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.spring.qna.dao.vo.Qna;
+import org.springframework.test.context.web.WebAppConfiguration;
 import com.spring.qna.service.QnaService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
+@WebAppConfiguration
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml",
 		"file:src/main/webapp/WEB-INF/spring/root-context.xml",
-		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+		"file:src/main/webapp/WEB-INF/spring/db-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/security-context.xml"})
 public class QnaControllerTest {
 	
 	@Autowired
 	QnaService qnaService;
 	
-	private Qna qna = new Qna();
-	
 	@Before
 	public void init() throws Exception {
-		String title = "ABC";
-		String content = "CBA";
-		String author = "Jack";
-		Date regdate = new Date();
-		long hit = 123456;
-		int likes = 12;
-		int pinned = 1;
-		String type = "car";
-		String author_email = "abc@abc.com";
-		
-		qna.setTitle(title);
-		qna.setContent(content);
-		qna.setRegdate(regdate);
-		qna.setAuthor(author);
-		qna.setHit(hit);
-		qna.setLikes(likes);
-		qna.setPinned(pinned);
-		qna.setType(type);
-		qna.setAuthor_email(author_email);
+
 	}
 
 	@Test
-	public void insert() throws Exception {
-		qnaService.create(qna);
+	public void read() throws Exception {
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap = qnaService.getDetail(161);
+		assertEquals("관리자", resultMap.get("AUTHOR"));
 	}
 }
